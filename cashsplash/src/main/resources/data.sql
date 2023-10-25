3a276eff0c3fe5b41aefc3bd8c0b14993285a843
devhebert <contatohebertluiz@gmail.com>
2023-10-24 21:20:53 -0300

    adiciona Build SQL

---
+++ cashsplash/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO users (email, username, password, role)
+VALUES ('admin@admin.com', 'admin', '$2a$12$UiJcvinaAY4i95L.fWKHgO26KLMQ/ridMaJBks9BCbm949av8tFle', 'ADMIN');
+INSERT INTO users (email, username, password, role)
+VALUES ('joao.santos@email.com', 'joao.santos', '$2a$12$UiJcvinaAY4i95L.fWKHgO26KLMQ/ridMaJBks9BCbm949av8tFle', 'ADMIN');
+INSERT INTO users (email, username, password, role)
+VALUES ('ana.pereira@email.com', 'ana.pereira', '$2a$12$UiJcvinaAY4i95L.fWKHgO26KLMQ/ridMaJBks9BCbm949av8tFle', 'COMMON');
+INSERT INTO users (email, username, password, role)
+VALUES ('pedro.oliveira@email.com', 'pedro.oliveira', '$2a$12$UiJcvinaAY4i95L.fWKHgO26KLMQ/ridMaJBks9BCbm949av8tFle', 'COMMON');
+INSERT INTO users (email, username, password, role)
+VALUES ('laura.costa@email.com', 'laura.costa', '$2a$12$UiJcvinaAY4i95L.fWKHgO26KLMQ/ridMaJBks9BCbm949av8tFle', 'COMMON');
+INSERT INTO users (email, username, password, role)
+VALUES ('luis.rodrigues@email.com', 'luis.rodrigues', '$2a$12$UiJcvinaAY4i95L.fWKHgO26KLMQ/ridMaJBks9BCbm949av8tFle', 'COMMON');
+
+
